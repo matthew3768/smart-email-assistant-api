c3f1c292403f91659559ae14f5801d10d8af65f0
matthew3768 <matthewimm@outlook.com>
2026-05-20 16:31:49 +0100

    Add reply tone options

--- app/main.py
+++ app/main.py
@@ -17,14 +17,33 @@ def health_check():
 
 @app.post("/draft-reply", response_model=EmailDraftResponse)
 def draft_reply(request: EmailDraftRequest):
-    suggetsted_reply = (
-        f"hi {request.sender},\n\n"
-        f"Thank you for your email about '{request.subject}'."
-        f"I'll review your message and get back to you shortly.\n\n"
-        f"Best Regards"
-    )
+    if request.tone == "friendly":
+        suggested_reply= (
+            f"Hi {request.sender},\n\n"
+            f"Thanks for reaching out about '{request.subject}'."
+            f"I'll take a look and get back to you soon.\n\n"
+            f"Best"
+
+        )
+
+    elif request.tone == "concise":
+        suggested_reply = (
+            f"Hi {request.sender},\n\n"
+            f"Thanks for your email. I'll review this and respond shortly.\n\n"
+            f"Best"
+        )
+
+    else:
+        suggested_reply = (
+            f"Hi {request.sender},\n\n"
+            f"Thank you for your email about '{request.subject}'."
+            f"I'll review and get back to you shortly.\n\n"
+            f"Best regards"
+        )
+    
 
     return EmailDraftResponse(
-        suggested_reply=suggetsted_reply,
+        subject=request.subject,
+        suggested_reply=suggested_reply,
         tone=request.tone,
     )   
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
+from enum import Enum
+
+class EmailReplyTone(str, Enum):
+    freindly = "friendly"
+    professional = "professional"
+    concise = "concise"
 
 class EmailDraftRequest(BaseModel):
     sender: str
     subject: str
     body: str
-    tone: str = "professional"
+    tone: EmailReplyTone = EmailReplyTone.professional
 
 class EmailDraftResponse(BaseModel):
     suggested_reply: str
-    tone: str
+    tone: EmailReplyTone
